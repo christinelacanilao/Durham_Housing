60ccb53078cb937fdde3252b9ebf8fe17f444941
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-06-06 13:54:46 -0400

    Update HomeButton.js

--- app/widget/js/HomeButton.js
+++ app/widget/js/HomeButton.js
@@ -4,6 +4,22 @@ require([
   
 ], function(WebMap, Home) {
 
+  // Creates a WebMap instance
+  var webmap = new WebMap({
+    portalItem: { //autocasts as new PortalItem()
+      id: "a37abf36893f42bbaccb0cef64fb28ab"
+    }
+  });
+      
+  // Mapview, referencing WebMap instance
+  var view = new MapView({
+    map: webmap,    // The WebMap instance created above
+      container: "viewDiv",
+      center: [-78.871866,43.914656],
+      zoom: 10
+  });
+  
+  // Home Widget
   var home = new HomeButton({
     view: view
   });
