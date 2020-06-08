f2a846c49397b49de77a5daef0a16f49197ec491
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-06-08 16:56:24 -0400

    Update HomeButton.js

--- app/widget/js/HomeButton.js
+++ app/widget/js/HomeButton.js
@@ -22,8 +22,8 @@ require([
   // Home Widget
   var home = new HomeButton({
     view: view
-  });
 
   // Add the widget to the left corner of the view
-  view.ui.add(home, "top-left");
+    view.ui.add(home, "top-left");
+  });
 });
