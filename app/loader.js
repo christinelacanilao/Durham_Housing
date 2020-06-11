5421f35f6972f9d78d856dc4e6e8cd29e1144740
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-06-11 14:01:04 -0400

    Update loader.js

--- app/loader.js
+++ app/loader.js
@@ -1,15 +1,16 @@
 // Obtained from https://www.geeksforgeeks.org/how-to-show-page-loading-div-until-the-page-has-finished-loading/
 
-document.onreadystatechange = function() { 
-            if (document.readyState !== "complete") { 
-                document.querySelector( 
-                  "body").style.visibility = "hidden"; 
-                document.querySelector( 
-                  "#loader").style.visibility = "visible"; 
-            } else { 
-                document.querySelector( 
-                  "#loader").style.display = "none"; 
-                document.querySelector( 
-                  "body").style.visibility = "visible"; 
-            } 
-        }; 
+document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+                        document.querySelector(
+                                    "body").style.visibility = "hidden";
+                        document.querySelector(
+                                    "#loader").style.visibility = "visible";
+            } else {
+                        document.querySelector(
+                                    "#loader").style.display = "none";
+                        document.querySelector(
+                                    "body").style.visibility = "visible";
+            }
+};
+                                    
