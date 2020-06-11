6beccc8490e22ae7402c25e4e8a303168ca54164
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-06-11 13:48:19 -0400

    Create loader.js

---
+++ app/loader.js
@@ -0,0 +1,15 @@
+// Obtained from https://www.geeksforgeeks.org/how-to-show-page-loading-div-until-the-page-has-finished-loading/
+
+document.onreadystatechange = function() { 
+            if (document.readyState !== "complete") { 
+                document.querySelector( 
+                  "body").style.visibility = "hidden"; 
+                document.querySelector( 
+                  "#loader").style.visibility = "visible"; 
+            } else { 
+                document.querySelector( 
+                  "#loader").style.display = "none"; 
+                document.querySelector( 
+                  "body").style.visibility = "visible"; 
+            } 
+        }; 
